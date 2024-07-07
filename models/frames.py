83bad3e0885c45f1c2548a8872a56f4d906091f8
Diego Bazalar Reyes <diegosud123423@gmail.com>
2024-07-06 23:51:35 -0500

    Undecimo commit

--- models/frames.py
+++ models/frames.py
@@ -11,6 +11,11 @@ class Trabajador(tk.Frame):
             super().__init__(parent)
             self.configure(bg=cfg.COLOR_CUERPO_PRINCIPAL)
             self.init_widgets_trabajador()
+            self.line_chart_canvas = None
+            self.line_chart_ax = None
+            self.circular_chart_canvas = None
+            self.circular_chart_ax = None
+            self.table = None
       
       def init_widgets_trabajador(self):
             self.frames_trabajador_top = {}
@@ -53,47 +58,50 @@ class Trabajador(tk.Frame):
                   
             labels=["Pagado","No Pagado","Pendiente"]
             data = [pagado, no_pagado, pendiente]
-            Graficos.create_grafico_circular(self.frame_bottom_circular, labels, data,"Trabajadores Pagados por Periodo")
+
+            self.circular_chart_canvas, self.circular_chart_ax = Graficos.create_grafico_circular(
+                  self.frame_bottom_circular, labels, data, "Trabajadores Pagados por Periodo", "Estados")
+
 
             self.frame_bottom_table = tk.Frame(master=self.frames_trabajador_bottom[1])
             self.frame_bottom_table.pack(padx=5,pady=5,fill=tk.BOTH,expand=True)
 
             table_columns = ["NUMERO","DNI", "NOMBRE", "MONTO", "Estado"]
-
             table_data = db_connection.ejecutar_usp_Trabajadores_cancelados('042024')
-
-            Graficos.create_grafico_table(self.frame_bottom_table,table_columns,table_data)
+            self.table = Graficos.create_grafico_table(self.frame_bottom_table, table_columns, table_data)
 
 class Pagos(tk.Frame):
       
       def __init__(self, parent, controller):
             super().__init__(parent)
             self.configure(bg=cfg.COLOR_CUERPO_PRINCIPAL)
+            self.data_cancelados = db_connection.ejecutar_usp_Trabajadores_cancelados('042024')#cambiar valor hard
+            dframeCancelados = pd.DataFrame(self.data_cancelados)
             self.option_selected = tk.StringVar()
-            self.cuerpo_principal()
-            self.init_widgets_Pagos()
+            self.option_selected.trace('w', self.update_chart)  # Detectar cambios en la opción seleccionada
+            self.cuerpo_principal(dframeCancelados)
+            self.init_widgets_Pagos(dframeCancelados)
+            self.line_chart_canvas = None
+            self.line_chart_ax = None
+            self.circular_chart_canvas = None
+            self.circular_chart_ax = None
+            self.table = None
 
-
-      def cuerpo_principal(self):
+      def cuerpo_principal(self,dframeCancelados):
+            print(dframeCancelados)
             self.cuerpo_principal_top=ctk.CTkFrame(master=self, fg_color="white")
             self.cuerpo_principal_top.pack(padx=5,pady=5, anchor = "w" ,side=tk.TOP)
-            valores=["casa","frijol","tuvieaja"],
-            self.option_selected.set("casadefrijo")
             #realizar la creación de un input tipo 
-            ctk.CTkOptionMenu(self.cuerpo_principal_top,self.option_selected,*valores,
-                                    fg_color="#F5E89D",
-                                    text_color="black",
-                                    button_color="#F5E88C",
-                                    button_hover_color="#F5E874",
-                                    height=30
-                                    ).grid(
-                                              row=0,
-                                              column=0,
-                                              padx=5,
-                                              pady=5
-                                              )
-            self.data_cancelados = db_connection.ejecutar_usp_Trabajadores_cancelados('042024')
-            dframeCancelados = pd.DataFrame(self.data_cancelados)
+            valores = ["052024","042024","032024"]
+            self.option_menu = ctk.CTkOptionMenu(self.cuerpo_principal_top,
+                                                 values = valores,
+                                                 fg_color="#F5E89D",
+                                                 text_color="black",
+                                                 button_color="#F5E88C",
+                                                 button_hover_color="#F5E874",
+                                                 variable = self.option_selected  #Asociar la variable
+                                                 )
+            self.option_menu.grid(row=0,column=0,padx=5,pady=5)
             
             self.frame_dialog = ctk.CTkFrame(master=self.cuerpo_principal_top, fg_color="#F5D971",border_width=1,border_color="white")
             self.frame_dialog.grid(row=0,column=1,padx=5,pady=5)
@@ -101,14 +109,9 @@ class Pagos(tk.Frame):
             ctk.CTkLabel(master=self.frame_dialog,
                               text="MONTO TOTAL DE PAGOS: ${}".format(dframeCancelados[3].sum()),
                               text_color="black", width=10, height=30
-                        ).grid(
-                              row=0,
-                              column=0,
-                              padx=5,
-                              pady=5
-                              )
+                        ).grid(row=0,column=0,padx=5,pady=5)
 
-      def init_widgets_Pagos(self):
+      def init_widgets_Pagos(self,dframeCancelados):
             #realizar la creación de una variable
 
             self.frame_top = tk.Frame(master=self, bg=COLOR_CUERPO_PRINCIPAL)
@@ -116,7 +119,7 @@ class Pagos(tk.Frame):
             
             self.frames_pagos_top = {}#array de los frames del top
 
-            for F in range(1): #creamos los 2 frames que contiene el frame top
+            for F in range(1): #creamos 1 frame que contiene el frame top
                   frame = ctk.CTkFrame(master=self.frame_top, fg_color="#FFFFFF")
                   self.frames_pagos_top[F] = frame
                   frame.pack(pady=5,padx=5,side=tk.TOP ,fill=tk.BOTH, expand = True)
@@ -126,9 +129,11 @@ class Pagos(tk.Frame):
             
             labels = ['012024', '022024', '032024', '042024', '052024']
             montos = [23455, 23434, 23353, 35454, 345456]
-            Graficos.create_grafico_line(self.frame_top_barras,montos, labels, "periodos","montos","Grafico De Lineas")
+            self.line_chart_canvas, self.line_chart_ax = Graficos.create_grafico_line(
+                        self.frame_top_barras, montos, labels, "periodos", "montos", "Grafico De Lineas")
+            
+            ##SECCIÓN INFERIOR
 
-##SECCIÓN INFERIOR
             self.frames_pagos_bottom = {}#array de los frames del bottom
 
             self.frame_bottom =tk.Frame(master=self,bg=COLOR_CUERPO_PRINCIPAL)
@@ -144,27 +149,47 @@ class Pagos(tk.Frame):
 
             table_columns = ["NUMERO","DNI", "NOMBRE", "MONTO", "Estado"]
             table_data = self.data_cancelados
-            Graficos.create_grafico_table(self.frame_table,table_columns,table_data)
+            self.table = Graficos.create_grafico_table(self.frame_table, table_columns, table_data)
 
             self.frame_bottom_circular = tk.Frame(master=self.frames_pagos_bottom[1], background="white")
             self.frame_bottom_circular.pack(padx=3,pady=3,side=tk.RIGHT, fill=tk.BOTH, expand= True)
-                                                        
-            pagado = 0
-            pendiente = 0
-            no_pagado = 0
-            for e in self.data_cancelados: 
-                 # print(e[4])
-                  if e[4] =="PAGADO":
-                        pagado += 1
-                  elif e[4] =="PENDIENTE":
-                        pendiente += 1
-                  elif e[4] =="NO PAGADO":
-                        no_pagado +=1
-                  
+                                                                
             labels=["Pagado","No Pagado","Pendiente"]
-            data = [pagado, no_pagado, pendiente]
 
-            Graficos.create_grafico_circular(self.frame_bottom_circular, labels, data,"Trabajadores Pagados por Periodo","Estados")
+            self.circular_chart_canvas, self.circular_chart_ax = Graficos.create_grafico_circular(
+                  self.frame_bottom_circular, labels, dframeCancelados[4].value_counts(), "Trabajadores Pagados por Periodo", "Estados")
+
+      def update_chart(self, *args):
+            selected_period = self.option_selected.get()
+            
+            self.data_cancelados = db_connection.ejecutar_usp_Trabajadores_cancelados(selected_period)
+            dframeCancelados = pd.DataFrame(self.data_cancelados)
+           
+            for widget in self.frame_dialog.winfo_children():
+                  widget.destroy()
+                  
+            ctk.CTkLabel(master=self.frame_dialog,
+                              text="MONTO TOTAL DE PAGOS: ${}".format(dframeCancelados[3].sum()),
+                              text_color="black", width=10, height=30
+                        ).grid(row=0,column=0,padx=5,pady=5)        
+
+            #Grafico de barras
+            labels = ['012024', '022024', '032024', '042024', '052024']
+            montos = [23455, 23434, 23353, 35454, 345456]
+
+            for widget in self.frame_top_barras.winfo_children():
+                  widget.destroy()
+            self.line_chart_canvas, self.line_chart_ax = Graficos.create_grafico_line(
+                        self.frame_top_barras, montos, labels, "periodos", "montos", "Grafico De Lineas")
+            
+            #Grafico Circular
+            labels = ["Pagado", "No Pagado", "Pendiente"]
+
+            for widget in self.frame_bottom_circular.winfo_children():
+                  widget.destroy()
+
+            self.circular_chart_canvas, self.circular_chart_ax = Graficos.create_grafico_circular(
+                  self.frame_bottom_circular, labels, dframeCancelados[4].value_counts(), "Trabajadores Pagados por Periodo", "Estados")
 
 
 class Sedes(tk.Frame):
--- models/graphs.py
+++ models/graphs.py
@@ -9,87 +9,99 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 class Graficos():
       def __init__():
          super().__init__()
-
-      def create_grafico_line(frame,data,labels,xlabel,ylabel,title):
-         # Crear la figura de matplotlib
-         fig = Figure(figsize=(1, 2), dpi=111)
-         ax = fig.add_subplot(111)
-         ax.plot(labels, data, "o-b")
-
-         ax.set_title('{}'.format(title),fontsize=10)
-
-         # Crear el canvas de tkinter para la figura de matplotlib
-         canvas = FigureCanvasTkAgg(fig, master=frame)
-         canvas.draw()
-         canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
- 
-
-      def create_grafico_circular(frame, labels, data, title, title_legend="leyenda", tipo=1):
-         fig, ax = plt.subplots(figsize=(5, 2), subplot_kw=dict(aspect="equal"), facecolor="white")
-         
-         colors = ["#6698F6", "#6EE1F8", "#F7FAA3"]
-         wedgeprops = {"edgecolor": "white", "linewidth":1}
-
-         wedges, texts, autotexts = ax.pie(data, 
-                                          autopct=lambda pct: f"{pct:.1f}%\n({int(np.round(pct / 100. * np.sum(data))):d})",
-                                          textprops=dict(color="black"),
-                                          colors=colors,
-                                          wedgeprops=wedgeprops)
-         
-         ax.legend(wedges, labels, title="{}".format(title_legend), loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
-         plt.setp(autotexts, size=8, weight="bold")
-         ax.set_title("{}".format(title))
-
-         if tipo == 2:
-             plt.show()
-         
-         if tipo == 1:
-            canvas = FigureCanvasTkAgg(fig, master=frame)
-            canvas.draw()
-            canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
-            
-            def on_hover(event):
-               hovered = False
-               for i, wedge in enumerate(wedges):
-                     if wedge.contains_point([event.x, event.y]):
-                        angle = (wedge.theta2 + wedge.theta1) / 2  # Calculate the middle angle of the wedge
-                        x_offset = 0.1 * np.cos(np.deg2rad(angle))  # Calculate the x offset
-                        y_offset = 0.1 * np.sin(np.deg2rad(angle))  # Calculate the y offset
-                        wedge.set_center((x_offset, y_offset))  # Move the wedge center
-                        wedge.set_radius(1.1)  # Highlight the hovered wedge by moving it outwards
-                        autotexts[i].set_fontsize(12)  # Increase the font size of the text
-                        canvas.draw()
-                        hovered = True
-                     else:
-                        wedge.set_radius(1.0)  # Reset all wedges to their original position
-                        wedge.set_center((0, 0))  # Reset center to original position
-                        autotexts[i].set_fontsize(8)  # Reset font size of the text
-               if not hovered:
-                     for autotext in autotexts:
-                        autotext.set_fontsize(8)  # Ensure all text is reset to normal size if not hovered
-                     canvas.draw()
-
-            canvas.mpl_connect("motion_notify_event", on_hover)
-
-
-      def create_grafico_table(frame, table_columns, table_data):
-         table = ttk.Treeview(master=frame, columns=table_columns, show ='headings')
-
-         for column in table_columns:
-            table.heading(column=column, text=column)
-            table.column(column=column,width=70)
-         if len(table_data) > 0:
-            for row_data in table_data:
-               table.insert(parent="",index="end", values=row_data)
+      @staticmethod
+      def create_grafico_line(frame, data, labels, xlabel, ylabel, title, canvas=None, ax=None):
+         if canvas and ax:
+               # Actualizar los datos del gráfico existente
+               ax.clear()
+               ax.plot(labels, data, "o-b")
+               ax.set_title(title, fontsize=10)
+               ax.set_xlabel(xlabel)
+               ax.set_ylabel(ylabel)
+               canvas.draw()
          else:
-            table.insert(parent="",index="end", values="No hay data que mostrar")
+               # Crear la figura de matplotlib
+               fig = Figure(figsize=(5, 2), dpi=100)
+               ax = fig.add_subplot(111)
+               ax.plot(labels, data, "o-b")
+               ax.set_title(title, fontsize=10)
+               ax.set_xlabel(xlabel)
+               ax.set_ylabel(ylabel)
+               # Crear el canvas de tkinter para la figura de matplotlib
+               canvas = FigureCanvasTkAgg(fig, master=frame)
+               canvas.draw()
+               canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+         return canvas, ax
+      
+      @staticmethod
+      def create_grafico_circular(frame, labels, data, title, title_legend="leyenda", canvas=None, ax=None):
+            if canvas and ax:
+                  # Actualizar los datos del gráfico existente
+                  ax.clear()
+                  wedges, texts, autotexts = ax.pie(
+                     data, autopct=lambda pct: f"{pct:.1f}%\n({int(np.round(pct / 100. * np.sum(data))):d})",
+                     textprops=dict(color="black"), colors=["#6698F6", "#6EE1F8", "#F7FAA3"],
+                     wedgeprops={"edgecolor": "white", "linewidth": 1}
+                  )
+                  ax.legend(wedges, labels, title=title_legend, loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
+                  ax.set_title(title)
+                  canvas.draw()
+            else:
+                  fig, ax = plt.subplots(figsize=(5, 2), subplot_kw=dict(aspect="equal"), facecolor="white")
+                  wedges, texts, autotexts = ax.pie(
+                     data, autopct=lambda pct: f"{pct:.1f}%\n({int(np.round(pct / 100. * np.sum(data))):d})",
+                     textprops=dict(color="black"), colors=["#6698F6", "#6EE1F8", "#F7FAA3"],
+                     wedgeprops={"edgecolor": "white", "linewidth": 1}
+                  )
+                  ax.legend(wedges, labels, title=title_legend, loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
+                  ax.set_title(title)
+                  canvas = FigureCanvasTkAgg(fig, master=frame)
+                  canvas.draw()
+                  canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-         style = ttk.Style()
-         style.theme_use('default')
-         #style.configure("Treeview", background="white", fieldbackground="white", foreground ="black")
-         style.configure("Treeview.Heading", background="#6E72F5", fieldbackground="#6E72F5", foreground ="white")
-         style.map('Treeview', background=[("selected","#6E72F5")])
-         table.pack(side=tk.TOP,fill=tk.BOTH,expand=True)
-            
+                  def on_hover(event):
+                     hovered = False
+                     for i, wedge in enumerate(wedges):
+                        if wedge.contains_point([event.x, event.y]):
+                              angle = (wedge.theta2 + wedge.theta1) / 2  # Calculate the middle angle of the wedge
+                              x_offset = 0.1 * np.cos(np.deg2rad(angle))  # Calculate the x offset
+                              y_offset = 0.1 * np.sin(np.deg2rad(angle))  # Calculate the y offset
+                              wedge.set_center((x_offset, y_offset))  # Move the wedge center
+                              wedge.set_radius(1.1)  # Highlight the hovered wedge by moving it outwards
+                              autotexts[i].set_fontsize(12)  # Increase the font size of the text
+                              canvas.draw()
+                              hovered = True
+                        else:
+                              wedge.set_radius(1.0)  # Reset all wedges to their original position
+                              wedge.set_center((0, 0))  # Reset center to original position
+                              autotexts[i].set_fontsize(8)  # Reset font size of the text
+                     if not hovered:
+                        for autotext in autotexts:
+                              autotext.set_fontsize(8)  # Ensure all text is reset to normal size if not hovered
+                        canvas.draw()
+
+                  canvas.mpl_connect("motion_notify_event", on_hover)
+            return canvas, ax
+
+      @staticmethod
+      def create_grafico_table(frame, table_columns, table_data, table=None):
+         if table:
+               # Limpiar la tabla existente y actualizar con los nuevos datos
+               for row in table.get_children():
+                  table.delete(row)
+               for row_data in table_data:
+                  table.insert("", "end", values=row_data)
+         else:
+               table = ttk.Treeview(master=frame, columns=table_columns, show='headings')
+               for column in table_columns:
+                  table.heading(column, text=column)
+                  table.column(column, width=70)
+               for row_data in table_data:
+                  table.insert("", "end", values=row_data)
+               style = ttk.Style()
+               style.theme_use('default')
+               style.configure("Treeview.Heading", background="#6E72F5", fieldbackground="#6E72F5", foreground="white")
+               style.map('Treeview', background=[("selected", "#6E72F5")])
+               table.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+         return table            
           
